547d99d432ae4ea3053f2e0634e03fd4abd56c45
removedporn <86824510+removedporn@users.noreply.github.com>
2023-02-22 21:22:04 +0800

    Delete app directory

--- app/controllers/visitors_controller.rb
+++
@@ -1,5 +0,0 @@
-class VisitorsController < ApplicationController
-	def index
-		@user = User.new
-	end
-end
--- app/models/user_document.rb
+++
@@ -1,4 +0,0 @@
-class UserDocument < ApplicationRecord
-  belongs_to :user, optional: true
-  belongs_to :document, optional: true
-end
